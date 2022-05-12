d57c65d3a9af374aa1a82513cad680c7aa5d6b3c
yehudz <yehuda.zeytim@gmail.com>
2022-05-11 18:05:45 -0600

    Desktop view

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import '../styles/main.scss'
+</script>
+
+<div class="container">
+  <div class="innerWrapper">
+    <div class="logo"></div>
+    <div class="contentContainer">
+      <div class="heroImage"></div>
+      <div class="content">
+        <div class="header">Build The Community Your Fans Will Love</div>
+        <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion. </p>
+        <div class="button">Register</div>
+      </div>
+    </div>
+    <div class="logos">
+      <div class="icon fb"></div>
+      <div class="icon tw"></div>
+      <div class="icon in"></div>
+    </div>
+  </div>
+</div>
